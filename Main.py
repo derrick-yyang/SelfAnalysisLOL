bc0a1cdee580b02c4f06f8bc403cf1784d9c67ce
derrick-yyang <yangguangzheng122@gmail.com>
2021-04-26 00:16:51 -0400

    Updated format of Main, RiotAPI, RiotParser

--- Main.py
+++ Main.py
@@ -1,69 +1,38 @@
 from RiotAPI import RiotAPI
 from RiotParser import RiotParser
-import pandas as pd
-
-
-def get_matchResult(matchData, name):
-
-    participantID = 0
-    for participantIdentities in matchData['participantIdentities']:
-        if name == participantIdentities['player']['summonerName']:
-            participantID = participantIdentities['participantId']
-    return matchData['participants'][participantID-1]['stats']['win']
-
-
-def get_winRate(matchList, name, api):
-
-    numberOfMatches = len(matchList['matches'])
-    numberOfWins = 0
-
-    for i in range(numberOfMatches):
-        if get_matchResult(api.get_match_by_matchid(matchList['matches'][i]['gameId']), name) == True:
-            numberOfWins += 1
-
-    if numberOfMatches != 0:
-        return numberOfWins/numberOfMatches*100
-    else:
-        return "Matchlist empty --"
-
+import time
 
 def main():
 
-    apikey = 'RGAPI-21a0bebc-f5f0-4c22-80c5-cfb9103a9b4e'  # API KEY IS HERE
+    apikey = 'RGAPI-72d0e930-a00d-4814-9b6f-071c988f6b71'  # API KEY IS HERE
     api = RiotAPI(apikey)
-    summonerName = input("Enter Summoner Name:")
-    summoner = api.get_summoner_by_name(summonerName)
+
+    # User input starts here:
+    summonerName = 'Controleed Freak'
 
     # begin index starts at 0, and end index is not included for the game number. Ex. 0->5 = First 5 games (0+1->5)
-    beginIndex = int(input('Enter beginIndex: '))
-    endIndex = int(input('Enter endIndex: '))
 
-    matchList = api.get_matchlist_by_account(
-        summoner['accountId'], {'beginIndex': beginIndex, 'endIndex': endIndex})
-    # print("Winrate (" + "Game " + str(beginIndex + 1) + " -> Game " + str(endIndex) +
-    #       ") : " + str(round(get_winRate(matchList, summonerName, api)))+"%")
-    # a = api.get_summoner_by_name('Controleed Freak')
-    print(summoner['profileIconId'])
+    beginCount = 0
+    endCount = 1
+    matchDataList = []
+    num = 10
+    for i in range(num):
+        matchDataList.append(api.get_matchDataList_by_account(summonerName, beginCount * 98, endCount *98))
+        beginCount += 1
+        endCount += 1
+        if i != num - 1:
+            time.sleep(121)
 
-    # add all match data into a list
-    matchData = []
-    for i in range(endIndex - beginIndex):
-        print(i)
-        matchData.append(api.get_match_by_matchid(
-            matchList['matches'][i]['gameId']))
+    parser = RiotParser(matchDataList[0], summonerName)
+    parsedDf = parser.parseData()
 
-    parser = RiotParser(matchData, summonerName)
-    parsedData = parser.parseData()
-    print(parsedData)
-    df = pd.DataFrame.from_dict(parsedData)
+    for i in range (1, num):
+        parser = RiotParser(matchDataList[i], summonerName)
+        newParsedDf = parser.parseData()
+        result = parsedDf.append(newParsedDf)
+        parsedDf = result
 
-    for i in range(len(parsedData["Summoner Name"])):
-        print(i)
-        rowLabelIndex = df.index[i]
-        df = df.rename(index={rowLabelIndex: "Match " + str(i + 1)})
-
-    df.to_csv('data.csv', mode='w')
-    
+    parsedDf.to_csv('Controleed Freak.csv', mode='w')
 
 if __name__ == "__main__":
     main()
--- RiotParser.py
+++ RiotParser.py
@@ -1,4 +1,3 @@
-
 # Given the match data from get_match_by_matchid from RiotAPI
 # Returns a panda Dataframe with the following column information (in this order):
 #   - Summoner name
@@ -9,6 +8,22 @@
 #   - Gold Total
 #   - Champion Level
 #   - Vision Score
+
+# TODO
+# Finish get_matchListData_by_account function in Riot API (remember to throttle)
+# Feed that data into this class (riotParser) and return a dataframe
+# Generate csv files for players
+
+# TODO2:
+# find winrate
+# get heat map
+
+# TODO3 (stretch goals):
+# color/scale the heatmap properly (40-60 instead of 0-100)
+# adjust how big the map is (not ugly)
+
+import pandas as pd
+
 
 class RiotParser(object):
 
@@ -28,6 +43,7 @@ class RiotParser(object):
     # Parses desired data (refer to above) from a list of matchData into a dictionary
     def parseData(self):
 
+        # add all match data into a list
         dataList = self.matchDataList
         parsedData = {self.SUMMONER_NAME: [],
                       self.GAME_DURATION: [],
@@ -40,7 +56,7 @@ class RiotParser(object):
                       }
 
         for i in range(len(dataList)):
-            if dataList[i]['mapId'] == 11 and dataList[i]['gameMode'] == 'CLASSIC':
+            if 'mapId' in dataList[i].keys() and dataList[i]['mapId'] == 11 and dataList[i]['gameMode'] == 'CLASSIC':
                 participantID = 0
                 for participantIdentities in dataList[i]['participantIdentities']:
                     if self.summonerName == participantIdentities['player']['summonerName']:
@@ -52,12 +68,23 @@ class RiotParser(object):
                     mr = 'Defeat'
                 # Creating the dictionary
                 parsedData[self.SUMMONER_NAME].append(self.summonerName)
-                parsedData[self.GAME_DURATION].append(dataList[i]['gameDuration'])
+                parsedData[self.GAME_DURATION].append(
+                    dataList[i]['gameDuration'])
                 parsedData[self.MATCH_RESULT].append(mr)
-                parsedData[self.CHAMPION_ID].append(dataList[i]['participants'][participantID - 1]['championId'])
-                parsedData[self.CREEP_SCORE].append(dataList[i]['participants'][participantID - 1]['stats']['totalMinionsKilled'])
-                parsedData[self.GOLD_EARNED].append(dataList[i]['participants'][participantID - 1]['stats']['goldEarned'])
-                parsedData[self.CHAMPION_LEVEL].append(dataList[i]['participants'][participantID - 1]['stats']['champLevel'])
-                parsedData[self.VISION_SCORE].append(dataList[i]['participants'][participantID - 1]['stats']['visionScore'])
+                parsedData[self.CHAMPION_ID].append(
+                    dataList[i]['participants'][participantID - 1]['championId'])
+                parsedData[self.CREEP_SCORE].append(
+                    dataList[i]['participants'][participantID - 1]['stats']['totalMinionsKilled'])
+                parsedData[self.GOLD_EARNED].append(
+                    dataList[i]['participants'][participantID - 1]['stats']['goldEarned'])
+                parsedData[self.CHAMPION_LEVEL].append(
+                    dataList[i]['participants'][participantID - 1]['stats']['champLevel'])
+                parsedData[self.VISION_SCORE].append(
+                    dataList[i]['participants'][participantID - 1]['stats']['visionScore'])
 
-        return parsedData
+        df = pd.DataFrame.from_dict(parsedData)
+        for i in range(len(parsedData["Summoner Name"])):
+            rowLabelIndex = df.index[i]
+            df = df.rename(index={rowLabelIndex: "Match " + str(i + 1)})
+
+        return df
